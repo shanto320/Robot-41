9354d814d58d74ab2a60f3aae594a807622eb084
naimkhuri <84196527+naimkhuri@users.noreply.github.com>
2021-06-25 15:13:44 -0300

    Update simple_navigation_goals.cpp

--- src/simple_navigation_goals.cpp
+++ src/simple_navigation_goals.cpp
@@ -15,13 +15,13 @@ int main(int argc, char** argv){
  //we'll send a goal to the robot to move 1 meter forward
  goal.target_pose.header.frame_id = "base_link";
  goal.target_pose.header.stamp = ros::Time::now();
- goal.target_pose.pose.position.x = 1.0;
- goal.target_pose.pose.orientation.w = 1.0;
+ goal.target_pose.pose.position.x = 1.0;                   //x coordinate
+ goal.target_pose.pose.orientation.w = 1.0;                //w coordinate
  ROS_INFO("Sending goal");
  ac.sendGoal(goal);
  ac.waitForResult();
  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
- ROS_INFO("Hooray, the base moved 1 meter forward");
+ ROS_INFO("Hooray, the base moved 1 meter forward");  //message dispaly
  else
  ROS_INFO("The base failed to move forward 1 meter for some reason");
  return 0;
